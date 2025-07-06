530485f391c4e5dbdf47cd9d9202a5f0f28e5a44
Raphael <68848614+r-2tanz4n1@users.noreply.github.com>
2025-07-06 15:58:16 +0100

    Tap dance 4
    a ver se acerto no keycode

--- keyboards/bastardkb/charybdis/4x6/keymaps/rstanzani/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/rstanzani/keymap.c
@@ -132,7 +132,7 @@ enum {
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
-    [TD_LALT] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, LALT),
+    [TD_LALT] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_LALT),
 };
 
 // Add tap dance item to your keymap in place of a keycode
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RBRC,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+       KC_LALT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, TD_LALT, KC_QUOT,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, SC_SENT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
